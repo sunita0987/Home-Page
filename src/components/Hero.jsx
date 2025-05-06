87b1e3f68999f0b360e2ea16ce206c6882732959
sunita0987 <sunita0987546@gmail.com>
2025-05-06 09:44:47 +0530

    heading change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ const Hero = () => {
   return (
     <div className="min-h-screen bg-[url(src/assets/images/bg.png)] bg-cover bg-center text-white">
       <nav className="flex items-center justify-between px-6 py-4">
-          <img src="src/assets/images/logo.png" alt="logo"></img>
+          <img src="src/assets/images/logo.png" alt="logo" className='ml-10'></img>
         <div className="hidden md:flex space-x-6 text-sm">
           <a href="#" className="hover:underline">About</a>
           <a href="#" className="hover:underline">How It Works</a>
@@ -12,15 +12,15 @@ const Hero = () => {
           <a href="#" className="hover:underline">Why Us</a>
           <a href="#" className="hover:underline">FAQ</a>
         </div>
-        <button className="bg-white text-blue-800 px-4 py-2 rounded-full font-semibold text-sm cursor-pointer hover:bg-gray-100">
+        <button className="bg-white text-blue-800 px-4 py-2 rounded-4xl font-semibold text-sm cursor-pointer hover:bg-gray-100">
           Get Started →
         </button>
       </nav>
       <div className="flex flex-col-reverse md:flex-row items-center justify-between px-6 md:px-16 py-12 md:py-20">
         <div className="max-w-xl">
-          <h1 className="text-4xl md:text-5xl font-bold mb-4">
+          <h1 className="text-4xl md:text-5xl font-normal mb-4">
             Easily Compare <br />
-            <span className="text-white">Energy, Gas, And <br/> Internet Plans</span>
+            <span className="text-white  font-bold">Energy, Gas, And <br/> Internet</span> <span className='font-normal'> Plans</span>
           </h1>
           <p className="text-lg text-gray-100 mb-6 font-helvetica">
             Find and switch to the best energy, gas, and internet plans with ease. 
@@ -33,7 +33,7 @@ const Hero = () => {
               placeholder="Start typing your address"
               className="flex-grow px-4 py-2 text-black rounded-l-full focus:outline-none font-helvetica"
             />
-            <button className="bg-blue-500 cursor-pointer hover:bg-blue-600 text-white px-6 py-2 rounded-full text-sm">
+            <button className="bg-blue-500 cursor-pointer hover:bg-blue-600 text-white px-6 py-2 rounded-4xl text-sm">
               Compare →
             </button>
           </div>
